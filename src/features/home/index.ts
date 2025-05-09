451ca3d8ed431420220e51f9a42966e435f303f5
Nahuel Frias <nahuelfrias@outlook.com.ar>
2025-05-09 19:15:21 -0300

    fix de la importacion en componente HOME

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,7 +1,7 @@
 // export { default as Home } from "./Home";
 // export { default as ClientsTable} from './ClientsTable';
-export { default as  ClientSummary } from './clientSummary'
-export { default as DashboardHeader } from './dashboardHeader'
+export { default as  ClientSummary } from './ClientSummary'
+export { default as DashboardHeader } from './DashboardHeader'
 export { default as QuickAccess } from './QuickAccess'
 export { default as TaskChecklist } from './TaskChecklist'
 export { default as UpcomingAppointments } from './UpcomingAppointments'
