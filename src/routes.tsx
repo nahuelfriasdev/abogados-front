3eedab676a59b2514f7bd407b0980fec08197bf1
Nahuel Frias <nahuelfrias@outlook.com.ar>
2025-06-15 14:38:28 -0300

    agrego ultimos cambios y habilito algunas pantalals

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import PrivateRoute from "./components/PrivateRoute";
-// import LoginPage from "./pages/LoginPage";
-// import Layout from "./components/Layout";
-// import ClientsPages from "./pages/ClientsPages";
-// import HomePage from "./pages/HomePages";
-// import PerfilPages from "./pages/PerfilPages";
-import LandingPages from "./pages/LandingPages";
-// import RegisterPage from "./features/landing/Register";
+import PrivateRoute from "./components/PrivateRoute";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./components/Layout";
+import ClientsPages from "./pages/ClientsPages";
+import HomePage from "./pages/HomePages";
+import PerfilPages from "./pages/PerfilPages";
+// import LandingPages from "./pages/LandingPages";
+import RegisterPage from "./features/landing/Register";
 import LayoutLanding from "./components/LayoutLanding";
 
 const AppRoutes = () => {
@@ -14,17 +14,17 @@ const AppRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LayoutLanding />}>
-          <Route path="/" element={<LandingPages />} />
-          {/* <Route path="/register" element={<RegisterPage />} />*/}
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
-        {/* <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<Layout />}>
             <Route path="/home" element={<HomePage />} />
             <Route path="/clients" element={<ClientsPages />} />
             <Route path="/client/:id" element={<PerfilPages />} />
           </Route>
-        </Route> */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
